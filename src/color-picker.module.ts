204b99055d47a722af053c362c71cf983e435be6
Andy Mepham <andymeps@yahoo.com>
2017-04-17 16:04:15 +0100

    Updated dependencies to ng4
    Resolved naming of ng2-bootstrap to ngx-bootsrap.

--- src/color-picker.module.ts
+++ src/color-picker.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ng2-bootstrap';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { ColorPickerComponent } from './components';
 
